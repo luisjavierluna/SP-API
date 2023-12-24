1f2fd9ea960d66ad529e227fca349a13543cc897
luisjavierluna <javierlunaluis@gmail.com>
2023-12-24 10:35:27 -0600

    Add base entity

---
+++ SP-API/Domain/Common/AuditableBaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public abstract class AuditableBaseEntity
+    {
+        public virtual int Id { get; set; }
+        public string Name { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string LastModifiedByAt { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
