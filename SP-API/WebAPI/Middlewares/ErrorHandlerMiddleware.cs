7eec71d22f401a1bf15dbcd4bb641d8168b66d37
luisjavierluna <javierlunaluis@gmail.com>
2023-12-25 16:18:19 -0600

    Add custom error middleware

---
+++ SP-API/WebAPI/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Application.Wrappers;
+using System.Net;
+using System.Text.Json;
+
+namespace WebAPI.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                var responseModel = new Response<string>() { Succeeded = false, Message = error?.Message };
+
+                switch (error)
+                {
+                    case Application.Exceptions.ApiException e:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest; 
+                        break;
+                    case Application.Exceptions.ValidationException e:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        responseModel.Errors = e.Errors;
+                        break;
+                    case KeyNotFoundException e:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+                var result = JsonSerializer.Serialize(responseModel);
+
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
--- SP-API/WebAPI/Program.cs
+++ SP-API/WebAPI/Program.cs
@@ -1,6 +1,7 @@
 using Application;
 using Persistence;
 using Shared;
+using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +27,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseErrorHandlingMiddleware();
 
 app.MapControllers();
 
