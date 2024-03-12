60a60a713aa3d61225675bfe20e2ab156da44382
luisjavierluna <javierlunaluis@gmail.com>
2024-03-11 18:03:42 -0600

    Configure initial settings for products module

---
+++ SP-UI/src/app/modules/products/products/product-list/product-list.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent {
+
+
+  departments = [
+    {
+      name: 'Humana',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatem veritatis corrupti tempore autem officia animi maxime atque corporis odit porro omnis eaque facilis eius consequatur delectus magni, fugiat repellendus.'
+    },
+    {
+      name: 'Humana',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatem veritatis corrupti tempore autem officia animi maxime atque corporis odit porro omnis eaque facilis eius consequatur delectus magni, fugiat repellendus.'
+    },
+    {
+      name: 'Humana',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatem veritatis corrupti tempore autem officia animi maxime atque corporis odit porro omnis eaque facilis eius consequatur delectus magni, fugiat repellendus.'
+    },
+    {
+      name: 'Humana',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatem veritatis corrupti tempore autem officia animi maxime atque corporis odit porro omnis eaque facilis eius consequatur delectus magni, fugiat repellendus.'
+    },
+    {
+      name: 'Humana',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatem veritatis corrupti tempore autem officia animi maxime atque corporis odit porro omnis eaque facilis eius consequatur delectus magni, fugiat repellendus.'
+    },
+    {
+      name: 'Humana',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatem veritatis corrupti tempore autem officia animi maxime atque corporis odit porro omnis eaque facilis eius consequatur delectus magni, fugiat repellendus.'
+    },
+    {
+      name: 'Humana',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatem veritatis corrupti tempore autem officia animi maxime atque corporis odit porro omnis eaque facilis eius consequatur delectus magni, fugiat repellendus.'
+    },
+    {
+      name: 'Humana',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatem veritatis corrupti tempore autem officia animi maxime atque corporis odit porro omnis eaque facilis eius consequatur delectus magni, fugiat repellendus.'
+    },
+  ]
+}
